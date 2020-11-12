ace6d058cccb8615011bac9f1175e5017a7342e2
Leif Sehlbach <leif.sehlbach@gmail.com>
2020-11-12 16:37:12 +0100

    cocktail features new created

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -9,8 +9,8 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
   def create
-    cocktail = Cocktail.new(cocktail_params)
-    cocktail.save
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save
     redirect_to cocktail_path(@cocktail)
   end
 
